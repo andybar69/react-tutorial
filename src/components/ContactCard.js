6a82dbfc23fcf71a5d51f0c8dcba102a078151b8
Andrei Barabash <a.barabash@itransition.com>
2020-07-24 00:03:09 +0300

    Initial commit

---
+++ src/components/ContactCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ContactCard extends Component {
+    render() {
+        return (
+            <div>ContactCard</div>
+        );
+    }
+}
+
+export default ContactCard;
---
+++ src/components/Navigation.js
@@ -0,0 +1,34 @@
+import React, {Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class Navigation extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <NavLink to='/' className="navbar-brand">Home</NavLink>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon">&nbsp;</span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <NavLink to="/todo-list" className="nav-link">Todo List</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/contact-card" className="nav-link">Contact Card</NavLink>
+                        </li>
+                    </ul>
+                    <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Navigation;
